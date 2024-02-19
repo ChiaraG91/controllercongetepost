25b29ca04db005bbfee71b265090402476aa9c0e
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-19 15:34:23 +0100

    Add files via upload

---
+++ controllercongetepost/src/main/java/com/controllercongetepost/controllercongetepost/ControllercongetepostApplication.java
@@ -0,0 +1,13 @@
+package com.controllercongetepost.controllercongetepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllercongetepostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllercongetepostApplication.class, args);
+	}
+
+}
---
+++ controllercongetepost/src/test/java/com/controllercongetepost/controllercongetepost/ControllercongetepostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controllercongetepost.controllercongetepost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllercongetepostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
